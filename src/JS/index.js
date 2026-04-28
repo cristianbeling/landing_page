ff5bbf4952056e53a5e1c5efa1ff4a112c51d7db
Cristian Beling <cristianbeling@hotmail.com>
2026-04-28 19:02:03 -0300

    feat: dando inicio ao JS

---
+++ src/JS/index.js
@@ -0,0 +1,4 @@
+const imagens = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+
