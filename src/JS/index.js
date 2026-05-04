2c999146f8b7a4565e84a419e1490aed0a8f47fb
Cristian Beling <cristianbeling@hotmail.com>
2026-05-04 09:56:07 -0300

    feat: adicionando a função avançar

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,4 +1,15 @@
-const imagens = document.querySelectorAll('.imagem-painel');
+const imagensPainel = document.querySelectorAll('.imagem-painel');
 const setaAvancar = document.getElementById('btn-avancar');
 const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+setaAvancar.addEventListener('click', function() {
+    imagemAtual++;
+
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar');
+    });
+
+    imagensPainel[imagemAtual].classList.add('mostrar');
+});
 
